41e787ff68dc56bf27c629e1ae54268e70d79129
jpawel01 <jacek.pawelec@isobar.com>
2014-09-03 14:31:36 +0200

    first part of model Generator/ModelGenerator.php

--- src/VisioCrudModeler/Generator/ModelGenerator.php
+++ src/VisioCrudModeler/Generator/ModelGenerator.php
@@ -9,26 +9,74 @@ namespace VisioCrudModeler\Generator;
  */
 class ModelGenerator implements GeneratorInterface
 {
+    /**
+     * holds params instance
+     *
+     * @var \VisioCrudModeler\Generator\ParamsInterface
+     */
+    protected $params = null;
+    
     /*
      * (non-PHPdoc)
      * @see \VisioCrudModeler\Generator\GeneratorInterface::generate()
      */
     public function generate(\VisioCrudModeler\Generator\ParamsInterface $params)
     {
-        if ($params->descriptor instanceof \VisioCrudModeler\Descriptor\ListGeneratorInterface) {
-            \Zend\Debug\Debug::dump($params->descriptor);
+        $this->params = $params;
+        
+        $descriptor = $this->params->getParam("descriptor");
+        
+        if (!($descriptor instanceof \VisioCrudModeler\Descriptor\ListGeneratorInterface)) {
             return;
         }
         
-        foreach ($this->descriptor->listGenerator() as $name=>$dataSet) {
-            echo $name;
-            if ($dataSet instanceof \VisioCrudModeler\Descriptor\ListGeneratorInterface) {
-                continue;
-            }
-            foreach ($dataSet->listGenerator() as $field=>$description) {
-                
-            }
+        foreach ($descriptor->listGenerator() as $name=>$dataSet) {
+            $this->generateBaseModel($name, $dataSet);
         }
         
+    }
+    
+    /**
+     * generates code for base model and saves in file.
+     * @param string $name
+     * @param \VisioCrudModeler\Descriptor\ListGeneratorInterface $dataSet
+     */
+    protected function generateBaseModel($name, \VisioCrudModeler\Descriptor\ListGeneratorInterface $dataSet)
+    {
+        $this->console($name);
+    }
+    
+    /**
+     * returns absolute path to module directory
+     *
+     * @return string
+     */
+    protected function moduleRoot()
+    {
+        $modulesDirectory = $this->params->getParam('modulesDirectory');
+        $moduleName = $this->params->getParam('moduleName');
+        return $modulesDirectory . DIRECTORY_SEPARATOR . $moduleName;
+    }
+
+    /**
+     * returns code library
+     *
+     * @return CodeLibrary
+     */
+    protected function codeLibrary()
+    {
+        return $this->params->getParam('di')->get('VisioCrudModeler\Generator\CodeLibrary');
+    }
+
+    /**
+     * proxy method for writing to console
+     *
+     * @param string $message            
+     */
+    protected function console($message)
+    {
+        if ($this->params->getParam('console') instanceof \Zend\Console\Adapter\AdapterInterface) {
+            $this->params->getParam('console')->writeLine($message);
+        }
     }
 }
